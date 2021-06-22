24a3265eefaea2d57cce9bd136a3d5f6cfe65b75
Anthony-WorkMac <fonganthonym@gmail.com>
2021-06-22 13:03:38 -0700

    Added pytesting file for testing initialformatting functions.

---
+++ basisdetection/preprocessing/tests/test_initialformatting.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" test_initialformatting.py
+Description: 
+"""
+__author__ = "Anthony Fong"
+__copyright__ = "Copyright 2021, Anthony Fong"
+__credits__ = ["Anthony Fong"]
+__license__ = ""
+__version__ = "1.0.0"
+__maintainer__ = "Anthony Fong"
+__email__ = ""
+__status__ = "Prototype"
+
+# Default Libraries #
+import pathlib
+
+# Downloaded Libraries #
+import pytest
+
+# Local Libraries #
+from ..initialformatting import *
+
+
+# Definitions #
+# Functions #
+@pytest.fixture
+def tmp_dir(tmpdir):
+    """A pytest fixture that turn the tmpdir into a Path object."""
+    return pathlib.Path(tmpdir)
+
+
+# Classes #
+class ClassTest:
+    """Default class tests that all classes should pass."""
+    class_ = None
+    timeit_runs = 100
+    speed_tolerance = 200
+
+    def get_log_lines(self, tmp_dir, logger_name):
+        path = tmp_dir.joinpath(f"{logger_name}.log")
+        with path.open() as f_object:
+            lines = f_object.readlines()
+        return lines
+
+
+# Main #
+if __name__ == '__main__':
+    pytest.main(["-v", "-s"])
